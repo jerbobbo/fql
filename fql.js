415024f5426aec65722372b82f549cdb58e3f849
Joel Darling <joel.darling@gmail.com>
2016-02-12 00:01:57 -0500

    Initial commit, 2 specs to go

--- fql.js
+++ fql.js
@@ -17,9 +17,125 @@ function _readTable (tableName) {
 	return table;
 }
 
-function merge (obj1, obj2) {}
+function merge (obj1, obj2) {
 
-function FQL (table) {}
+	var result = {};
+
+	for(var key in obj1){
+		result[key] = obj1[key];
+	}
+	for(key in obj2){
+		result[key] = obj2[key];
+	}
+
+	return result;
+}
+
+
+function FQL (table) {
+	this.table = table;
+	this.indexTables = {};
+}
+
+FQL.prototype.exec = function() {
+	return this.table;
+};
+
+FQL.prototype.count = function() {
+	return this.table.length;
+};
+
+FQL.prototype.limit = function(size){
+	return new FQL(this.table.slice(0, size));
+};
+
+FQL.prototype.where = function(object) {
+	var returnArr = [];
+	this.table.forEach(function(row) {
+
+		var numTrue = 0;
+
+		for (var key in object) {
+
+			if ( typeof object[key] === 'function') {
+				if (object[key](row[key]))
+					numTrue++;
+			}
+
+			else if (row[key] === object[key])
+				numTrue++;
+		}
+		if (numTrue === Object.keys(object).length)
+			returnArr.push(row);
+
+	});
+
+	return new FQL(returnArr);
+};
+
+FQL.prototype.select = function (cols){
+	var returnArr = [];
+
+	this.table.forEach(function(row){
+		var newObj = {};
+
+		for(var i=0;i<cols.length;i++){
+			newObj[cols[i]] = row[cols[i]];
+		}
+		returnArr.push(newObj);
+	});
+
+	return new FQL(returnArr);
+};
+
+FQL.prototype.order = function(param){
+
+	function sort(a, b){
+			if(a[param] > b[param])
+				return 1;
+			else if(a[param] < b[param])
+				return -1;
+			else
+				return 0;
+		}
+
+	return new FQL(this.table.sort(sort));
+};
+
+FQL.prototype.left_join = function(table_left, testFunc) {
+	var retArray = [];
+	var that = this;
+	table_left.table.forEach(function(row_left) {
+		that.table.forEach(function(row_right) {
+			if (testFunc(row_right, row_left))
+				retArray.push(merge(row_left, row_right));
+
+		});
+	});
+	return new FQL(retArray);
+};
+
+FQL.prototype.addIndex = function(column) {
+	var newIndex = {};
+	for (var i=0; i < this.table.length; i++){
+		var row = this.table[i];
+
+		if (!newIndex[row[column]]) {
+			newIndex[row[column]] = [];
+		}
+		newIndex[row[column]].push(i);
+
+	}
+
+	this.indexTables[column] = newIndex;
+};
+
+FQL.prototype.getIndicesOf = function(column, searchStr) {
+	if (!this.indexTables[column])
+		return undefined;
+	var index = this.indexTables[column];
+	return index[searchStr];
+};
 
 module.exports = {
 	FQL: FQL,
